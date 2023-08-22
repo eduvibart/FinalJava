04a8623ec6a192ee4f5c7c66467203876932d982
MatiasFino <mfino@alumnos.exa.unicen.edu.ar>
2023-08-22 17:42:58 -0300

    FileCorrector Pasa todos los Test

--- spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/FileCorrector.java
+++ spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/FileCorrector.java
@@ -3,6 +3,7 @@ package edu.isistan.spellchecker.corrector.impl;
 import edu.isistan.spellchecker.corrector.Corrector;
 import edu.isistan.spellchecker.tokenizer.TokenScanner;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -17,9 +18,10 @@ import java.util.TreeSet;
  */
 public class FileCorrector extends Corrector {
 
-	private Hashtable<String,Set<String>> correctionMap = new Hashtable<String,Set<String>>();
+	private Hashtable<String, Set<String>> correctionMap = new Hashtable<String, Set<String>>();
 
-	/** Clase especial que se utiliza al tener 
+	/**
+	 * Clase especial que se utiliza al tener
 	 * alg�n error de formato en el archivo de entrada.
 	 */
 	public static class FormatException extends Exception {
@@ -33,12 +35,13 @@ public class FileCorrector extends Corrector {
 	 *
 	 * Utilice un BufferedReader para leer el archivo de definici�n
 	 *
-	 * <p> 
-	 * Cada l�nea del archivo del diccionario tiene el siguiente formato: 
+	 * <p>
+	 * Cada l�nea del archivo del diccionario tiene el siguiente formato:
 	 * misspelled_word,corrected_version
 	 *
 	 * <p>
-	 *Ejemplo:<br>
+	 * Ejemplo:<br>
+	 * 
 	 * <pre>
 	 * aligatur,alligator<br>
 	 * baloon,balloon<br>
@@ -48,7 +51,9 @@ public class FileCorrector extends Corrector {
 	 * ther,there<br>
 	 * </pre>
 	 * <p>
-	 * Estas l�neas no son case-insensitive, por lo que todas deber�an generar el mismo efecto:<br>
+	 * Estas l�neas no son case-insensitive, por lo que todas deber�an generar el
+	 * mismo efecto:<br>
+	 * 
 	 * <pre>
 	 * baloon,balloon<br>
 	 * Baloon,balloon<br>
@@ -59,17 +64,21 @@ public class FileCorrector extends Corrector {
 	 * <p>
 	 * Debe ignorar todos los espacios vacios alrededor de las palabras, por lo
 	 * que estas entradas son todas equivalentes:<br>
+	 * 
 	 * <pre>
 	 * inspite,in spite<br>
 	 *    inspite,in spite<br>
 	 * inspite   ,in spite<br>
 	 *  inspite ,   in spite  <br>
 	 * </pre>
-	 * Los espacios son permitidos dentro de las sugerencias. 
+	 * 
+	 * Los espacios son permitidos dentro de las sugerencias.
 	 *
 	 * <p>
-	 * Deber�a arrojar <code>FileCorrector.FormatException</code> si se encuentra alg�n
+	 * Deber�a arrojar <code>FileCorrector.FormatException</code> si se encuentra
+	 * alg�n
 	 * error de formato:<br>
+	 * 
 	 * <pre>
 	 * ,correct<br>
 	 * wrong,<br>
@@ -78,53 +87,48 @@ public class FileCorrector extends Corrector {
 	 * </pre>
 	 * <p>
 	 *
-	 * @param r Secuencia de caracteres 
-	 * @throws IOException error leyendo el archivo
+	 * @param r Secuencia de caracteres
+	 * @throws IOException                   error leyendo el archivo
 	 * @throws FileCorrector.FormatException error de formato
-	 * @throws IllegalArgumentException reader es null
+	 * @throws IllegalArgumentException      reader es null
 	 */
 	public FileCorrector(Reader r) throws IOException, FormatException {
 		if (r == null)
 			throw new IllegalArgumentException();
-		else {
+		if (r instanceof FileReader){
 			TokenScanner token = new TokenScanner(r);
 			String nextToken;
 			String value;
-			while (token.hasNext()){
+			while (token.hasNext()) {
 				nextToken = token.next();
-				while (!token.isWord(nextToken)){
+				while (!token.isWord(nextToken)) {
 					nextToken = token.next();
 				}
 				value = token.next();
-				while (!token.isWord(value)){
+				while (token.hasNext() && !token.isWord(value)) {
 					value = token.next();
 				}
-				if (token.isWord(nextToken)){
-						if (correctionMap.containsKey(nextToken.toLowerCase())){
-							while (!token.isWord(value)){
-								value = token.next();
-							}
-							correctionMap.get(nextToken).add(value);
-				
-					} else {
-						Set<String> newSet = new TreeSet<String>();
-						while (!token.isWord(value)){
-								value = token.next();
-							}
-						newSet.add(value);
-						correctionMap.put(nextToken.toLowerCase(), newSet);
-						}
-					}
+				if (correctionMap.containsKey(nextToken.toLowerCase())) {
+					correctionMap.get(nextToken).add(value);
+
+				} else {
+					Set<String> newSet = new TreeSet<String>();
+					newSet.add(value);
+					correctionMap.put(nextToken.toLowerCase(), newSet);
 				}
 			}
 		}
+		else
+			throw new FormatException("Reader type must be java.io.BufferReader");
+	}
 
-	/** Construye el Filereader.
+	/**
+	 * Construye el Filereader.
 	 *
-	 * @param filename 
-	 * @throws IOException 
-	 * @throws FileCorrector.FormatException 
-	 * @throws FileNotFoundException 
+	 * @param filename
+	 * @throws IOException
+	 * @throws FileCorrector.FormatException
+	 * @throws FileNotFoundException
 	 */
 	public static FileCorrector make(String filename) throws IOException, FormatException {
 		Reader r = new FileReader(filename);
@@ -132,7 +136,9 @@ public class FileCorrector extends Corrector {
 		try {
 			fc = new FileCorrector(r);
 		} finally {
-			if (r != null) { r.close(); }
+			if (r != null) {
+				r.close();
+			}
 		}
 		return fc;
 	}
@@ -143,26 +149,26 @@ public class FileCorrector extends Corrector {
 	 * <p>
 	 * Ver superclase.
 	 *
-	 * @param wrong 
+	 * @param wrong
 	 * @return retorna un conjunto (potencialmente vac�o) de sugerencias.
-	 * @throws IllegalArgumentException si la entrada no es una palabra v�lida 
+	 * @throws IllegalArgumentException si la entrada no es una palabra v�lida
 	 */
 
 	public Set<String> getCorrections(String wrong) {
-		
+
 		Set<String> corrections = new TreeSet<String>();
-		if (wrong != null){
+		if (wrong != null) {
 			try {
 				corrections = correctionMap.get(wrong.toLowerCase());
 				return corrections;
 
 			} catch (NullPointerException e) {
 				System.out.println("nothing");
-				
+
 			} finally {
 				return matchCase(wrong, corrections);
-			} 
-		} else {	
+			}
+		} else {
 			throw new IllegalArgumentException();
 		}
 	}
