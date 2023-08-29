b46bb7b65497b1ba8dafc135a5ff02ebcec4e486
MatiasFino <mfino@alumnos.exa.unicen.edu.ar>
2023-08-29 17:32:18 -0300

    SwapCorrector Working

--- spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/SwapCorrector.java
+++ spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/SwapCorrector.java
@@ -1,14 +1,18 @@
 package edu.isistan.spellchecker.corrector.impl;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import edu.isistan.spellchecker.corrector.Corrector;
 import edu.isistan.spellchecker.corrector.Dictionary;
+
 /**
- * Este corrector sugiere correciones cuando dos letras adyacentes han sido cambiadas.
+ * Este corrector sugiere correciones cuando dos letras adyacentes han sido
+ * cambiadas.
  * <p>
- * Un error común es cambiar las letras de orden, e.g.
- * "with" -> "wiht". Este corrector intenta dectectar palabras con exactamente un swap.
+ * Un error comï¿½n es cambiar las letras de orden, e.g.
+ * "with" -> "wiht". Este corrector intenta dectectar palabras con exactamente
+ * un swap.
  * <p>
  * Por ejemplo, si la palabra mal escrita es "haet", se debe sugerir
  * tanto "heat" como "hate".
@@ -17,22 +21,29 @@ import edu.isistan.spellchecker.corrector.Dictionary;
  */
 public class SwapCorrector extends Corrector {
 
+	private Dictionary dict;
+
 	/**
 	 * Construcye el SwapCorrector usando un Dictionary.
 	 *
-	 * @param dict 
+	 * @param dict
 	 * @throws IllegalArgumentException si el diccionario provisto es null
 	 */
 	public SwapCorrector(Dictionary dict) {
-
+		if (dict == null)
+			throw new IllegalArgumentException();
+		else
+			this.dict = dict;
 	}
 
 	/**
 	 * 
-	 * Este corrector sugiere correciones cuando dos letras adyacentes han sido cambiadas.
+	 * Este corrector sugiere correciones cuando dos letras adyacentes han sido
+	 * cambiadas.
 	 * <p>
-	 * Un error común es cambiar las letras de orden, e.g.
-	 * "with" -> "wiht". Este corrector intenta dectectar palabras con exactamente un swap.
+	 * Un error comï¿½n es cambiar las letras de orden, e.g.
+	 * "with" -> "wiht". Este corrector intenta dectectar palabras con exactamente
+	 * un swap.
 	 * <p>
 	 * Por ejemplo, si la palabra mal escrita es "haet", se debe sugerir
 	 * tanto "heat" como "hate".
@@ -41,11 +52,24 @@ public class SwapCorrector extends Corrector {
 	 * <p>
 	 * Ver superclase.
 	 *
-	 * @param wrong 
-	 * @return retorna un conjunto (potencialmente vacío) de sugerencias.
-	 * @throws IllegalArgumentException si la entrada no es una palabra válida 
+	 * @param wrong
+	 * @return retorna un conjunto (potencialmente vacï¿½o) de sugerencias.
+	 * @throws IllegalArgumentException si la entrada no es una palabra vï¿½lida
 	 */
 	public Set<String> getCorrections(String wrong) {
-		return null;
+		char aux;
+		Set<String> suggestions = new HashSet<String>();
+		char[] word = wrong.toCharArray();
+		for (int i = 0; i < word.length - 1; i++) {
+			aux = word[i];
+			word[i] = word[i + 1];
+			word[i + 1] = aux;
+			String possibleWord = new String(word);
+			if (dict.isWord(possibleWord))
+				suggestions.add(possibleWord);
+			word[i + 1] = word[i];
+			word[i] = aux;
+		}
+		return matchCase(wrong, suggestions);
 	}
 }
--- spellchecker/src/test/java/edu/isistan/spellchecker/corrector/impl/FileCorrectorTest.java
+++ spellchecker/src/test/java/edu/isistan/spellchecker/corrector/impl/FileCorrectorTest.java
@@ -1,11 +1,10 @@
 package edu.isistan.spellchecker.corrector.impl;
+
 import static org.junit.Assert.*;
 
 import java.util.TreeSet;
 import java.util.Set;
 import java.io.*;
-
-
 
 import org.junit.Test;
 
@@ -13,7 +12,6 @@ import edu.isistan.spellchecker.corrector.Corrector;
 import edu.isistan.spellchecker.corrector.impl.FileCorrector;
 
 public class FileCorrectorTest {
-
 
   private Set<String> makeSet(String[] strings) {
     Set<String> mySet = new TreeSet<String>();
@@ -23,34 +21,31 @@ public class FileCorrectorTest {
     return mySet;
   }
 
-  
-  
-  @Test public void testFileCorrectorNullReader() throws IOException, FileCorrector.FormatException {
+  @Test
+  public void testFileCorrectorNullReader() throws IOException, FileCorrector.FormatException {
     try {
       new FileCorrector(null);
       fail("Expected an IllegalArgumentException - cannot create FileCorrector with null.");
-    } catch (IllegalArgumentException f) {    
-      //Do nothing. It's supposed to throw an exception
+    } catch (IllegalArgumentException f) {
+      // Do nothing. It's supposed to throw an exception
     }
   }
 
-  
-  @Test public void testGetCorrection() throws IOException, FileCorrector.FormatException  {
+  @Test
+  public void testGetCorrection() throws IOException, FileCorrector.FormatException {
     Corrector c = FileCorrector.make("smallMisspellings.txt");
-    assertEquals("lyon -> lion", makeSet(new String[]{"lion"}), c.getCorrections("lyon"));
+    assertEquals("lyon -> lion", makeSet(new String[] { "lion" }), c.getCorrections("lyon"));
     TreeSet<String> set2 = new TreeSet<String>();
-    assertEquals("TIGGER -> {Trigger,Tiger}", makeSet(new String[]{"Trigger","Tiger"}), c.getCorrections("TIGGER"));
+    assertEquals("TIGGER -> {Trigger,Tiger}", makeSet(new String[] { "Trigger", "Tiger" }), c.getCorrections("TIGGER"));
   }
 
-
-  
-  @Test public void testInvalidFormat() throws IOException, FileCorrector.FormatException  {
-	 try {
-		  Corrector c = new FileCorrector(new StringReader("no comma in this puppy"));
-	     fail("This is a bad format");
-	 } catch (FileCorrector.FormatException e) {
-	    // do nothing
-	 }
+  @Test
+  public void testInvalidFormat() throws IOException, FileCorrector.FormatException {
+    try {
+      Corrector c = new FileCorrector(new StringReader("no comma in this puppy"));
+      fail("This is a bad format");
+    } catch (FileCorrector.FormatException e) {
+      // do nothing
+    }
   }
-
 }
