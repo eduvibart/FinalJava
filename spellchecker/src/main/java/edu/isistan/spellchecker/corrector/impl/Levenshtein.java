a36edf313b66e028969d2fdd2f82e56cc8657ba0
MatiasFino <mfino@alumnos.exa.unicen.edu.ar>
2023-08-29 20:10:11 -0300

    All Corrector Implementations working properly

--- spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/Levenshtein.java
+++ spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/Levenshtein.java
@@ -1,4 +1,5 @@
 package edu.isistan.spellchecker.corrector.impl;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,23 +8,24 @@ import edu.isistan.spellchecker.corrector.Dictionary;
 
 /**
  *
- * Un corrector inteligente que utiliza "edit distance" para generar correcciones.
+ * Un corrector inteligente que utiliza "edit distance" para generar
+ * correcciones.
  * 
- * La distancia de Levenshtein es el número minimo de ediciones que se deber
- * realizar a un string para igualarlo a otro. Por edición se entiende:
+ * La distancia de Levenshtein es el nï¿½mero minimo de ediciones que se deber
+ * realizar a un string para igualarlo a otro. Por ediciï¿½n se entiende:
  * <ul>
- * <li> insertar una letra
- * <li> borrar una letra
- * <li> cambiar una letra
+ * <li>insertar una letra
+ * <li>borrar una letra
+ * <li>cambiar una letra
  * </ul>
  *
  * Una "letra" es un caracter a-z (no contar los apostrofes).
- * Intercambiar letras (thsi -> this) <it>no</it> cuenta como una edición.
+ * Intercambiar letras (thsi -> this) <it>no</it> cuenta como una ediciï¿½n.
  * <p>
  * Este corrector sugiere palabras que esten a edit distance uno.
  */
 public class Levenshtein extends Corrector {
-
+	private Dictionary dict;
 
 	/**
 	 * Construye un Levenshtein Corrector usando un Dictionary.
@@ -32,7 +34,10 @@ public class Levenshtein extends Corrector {
 	 * @param dict
 	 */
 	public Levenshtein(Dictionary dict) {
-		throw new UnsupportedOperationException(); // STUB
+		if (dict == null)
+			throw new IllegalArgumentException(); // STUB
+		else
+			this.dict = dict;
 	}
 
 	/**
@@ -40,7 +45,17 @@ public class Levenshtein extends Corrector {
 	 * @return todas las palabras a erase distance uno
 	 */
 	Set<String> getDeletions(String s) {
-		throw new UnsupportedOperationException(); // STUB
+		StringBuilder sBuilder = new StringBuilder(s);
+		Set<String> suggestions = new HashSet<String>();
+		char deletedChar;
+		for (int i = 0; i < s.length(); i++) {
+			deletedChar = sBuilder.charAt(i);
+			sBuilder.deleteCharAt(i);
+			if (dict.isWord(sBuilder.toString()))
+				suggestions.add(sBuilder.toString());
+			sBuilder.insert(i, deletedChar);
+		}
+		return suggestions;
 	}
 
 	/**
@@ -48,19 +63,48 @@ public class Levenshtein extends Corrector {
 	 * @return todas las palabras a substitution distance uno
 	 */
 	public Set<String> getSubstitutions(String s) {
-		throw new UnsupportedOperationException(); // STUB
+		Set<String> suggestions = new HashSet<String>();
+		StringBuilder possibleWord = new StringBuilder(s);
+		char originalLetter;
+		for (int i = 0; i < s.length(); i++) {
+			originalLetter = possibleWord.charAt(i);
+			for (char c = 'a'; c <= 'z'; c++) {
+				possibleWord.setCharAt(i, c);
+				if (!possibleWord.toString().equals(s))
+					if (dict.isWord(possibleWord.toString()))
+						suggestions.add(possibleWord.toString());
+			}
+			possibleWord.setCharAt(i, originalLetter);
+		}
+		return suggestions;
 	}
-
 
 	/**
 	 * @param s palabra
 	 * @return todas las palabras a insert distance uno
 	 */
 	public Set<String> getInsertions(String s) {
-		throw new UnsupportedOperationException(); // STUB
+		Set<String> suggestions = new HashSet<String>();
+		StringBuilder possibleWord = new StringBuilder(s);
+		for (int i = 0; i <= possibleWord.length(); i++) {
+			for (char c = 'a'; c <= 'z'; c++) {
+				possibleWord.insert(i, c);
+				if (dict.isWord(possibleWord.toString()))
+					suggestions.add(possibleWord.toString());
+				possibleWord.deleteCharAt(i);
+			}
+		}
+		return suggestions;
 	}
 
 	public Set<String> getCorrections(String wrong) {
-		throw new UnsupportedOperationException(); // STUB
+		if (wrong == null)
+			throw new IllegalArgumentException();
+		else{
+			Set<String> suggestions = getInsertions(wrong);
+			suggestions.addAll(getDeletions(wrong));
+			suggestions.addAll(getSubstitutions(wrong));
+			return matchCase(wrong, suggestions);	
+		}
 	}
 }
